384cd21dcf859954bf014126140d5b15086455fb
Austin Ellis <austinellis@gmail.com>
2016-12-13 00:45:34 -0800

    use db filter

--- project/app.py
+++ project/app.py
@@ -67,7 +67,7 @@ def get_job_route(job_id):
 def get_jobs_route():
     user = get_user()
     jobs = get_jobs(job_ids=user.get('jobs',[]))
-    return jsonify(jobs={x['id']:{'url':x['url'], 'id':x['id']} for x in jobs})
+    return jsonify(jobs={x['id']: x for x in jobs})
 
 @app.route('/fetch/', methods=['POST'])
 def fetch():
@@ -125,7 +125,7 @@ def get_job(job_id):
     return job
 
 def get_jobs(job_ids):
-    jobs = [x for x in db.jobs.find({'_id': {'$in': [ObjectId(y) for y in job_ids]}}, {'_id': 0})]
+    jobs = [x for x in db.jobs.find({'_id': {'$in': [ObjectId(y) for y in job_ids]}}, {'_id': 0, 'url':1, 'id':1})]
     return jobs
 
 def get_new_job(url):
